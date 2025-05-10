0cd43cf9f132e864885d2010e3105ce9c3b116cc
MilanMareels <milan.mareels@gmail.com>
2025-05-10 18:23:19 +0200

    gateway done

--- project/gateway/index.js
+++ project/gateway/index.js
@@ -3,14 +3,19 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 require("dotenv").config();
 
 const app = express();
-const port = 8080;
+const port = 5000;
 
-app.use("/auth", createProxyMiddleware({ target: "http://localhost:4000", changeOrigin: true }));
-app.use("/game-catalog", createProxyMiddleware({ target: "http://localhost:3000", changeOrigin: true }));
-app.use("/ratings", createProxyMiddleware({ target: "http://localhost:3001", changeOrigin: true }));
-app.use("/recommendations", createProxyMiddleware({ target: "http://localhost:3002", changeOrigin: true }));
-app.use("/notifications", createProxyMiddleware({ target: "http://localhost:3003", changeOrigin: true }));
-app.use("/users", createProxyMiddleware({ target: "http://localhost:3004", changeOrigin: true }));
+app.use("/login", createProxyMiddleware({ target: "http://auth-service:80/login", changeOrigin: true }));
+app.use("/game-catalog", createProxyMiddleware({ target: "http://game-catalog-service:80/game-catalog", changeOrigin: true }));
+app.use("/ratings", createProxyMiddleware({ target: "http://rating-service:80/ratings", changeOrigin: true }));
+app.use("/recommendations", createProxyMiddleware({ target: "http://recommendations-service:80/recommendations", changeOrigin: true }));
+app.use("/notifications", createProxyMiddleware({ target: "http://notifications-service:80/notifications", changeOrigin: true }));
+app.use("/users/me", createProxyMiddleware({ target: "http://users-service:80/users/me", changeOrigin: true }));
+app.use("/collection", createProxyMiddleware({ target: "http://user-collection-service:80/collection", changeOrigin: true }));
+
+app.use("/curator", createProxyMiddleware({ target: "http://curator-service:80/curator", changeOrigin: true }));
+app.use("/media", createProxyMiddleware({ target: "http://media-service:80/media", changeOrigin: true }));
+app.use("/prices", createProxyMiddleware({ target: "http://price-tracking-service:80/prices", changeOrigin: true }));
 
 app.listen(port, () => {
   console.log(`API Gateway running on http://localhost:${port}`);
